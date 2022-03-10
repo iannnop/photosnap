0b4aa915f2614894ab80f9ce1dac6b773038a8ad
Ian <iannnop@gmail.com>
2022-03-10 16:38:37 +1000

    added mobile nav functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,14 @@
 import { AnimatePresence } from "framer-motion"
+import { motion } from "framer-motion"
 import Image from "next/image"
 import Link from "next/link"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Button1 } from "./Buttons"
 import { Logo } from "./Icons"
 
 const Header = () => {
   return (
-    <nav className="max-w-7xl mx-auto px-6 py-4 md:px-10 flex flex-shrink justify-between items-center">
+    <nav className="fixed md:static bg-white w-full md:mx-auto px-6 py-4 md:px-10 inline-flex justify-between items-center z-50">
       <Logo textColor="#000"/>
       <div className="uppercase font-bold text-xs space-x-8 tracking-[2px] hidden sm:inline-block">
         <Link href='/stories'>
@@ -43,9 +44,43 @@ const MobileMenu = () => {
     }
   }
 
+  useEffect(() => {
+    return function cleanup() {
+      document.body.style.overflow = '';
+    };
+  }, []);
+
   return (
-    <button className="flex items-center sm:hidden">
-      <Image width={20} height={6} src='/assets/shared/mobile/menu.svg' alt='mobile menu'/>
-    </button>
+    <>
+      <button onClick={toggleMenu} aria-label="toggle-menu" className="absolute top-3 right-6 sm:hidden">
+        <span className={!isMenuOpen ? "" : "hidden"}>
+          <Image width={20} height={6} src='/assets/shared/mobile/menu.svg' alt='mobile nav menu'/>
+        </span>
+        <span className={isMenuOpen ? "" : "hidden"}>
+          <Image width={16} height={15} src="/assets/shared/mobile/close.svg" alt='close button' />
+        </span>
+      </button>
+      <AnimatePresence>
+        {isMenuOpen && (
+          <div>
+            <span className="absolute bg-black opacity-50 top-14 left-0 min-h-screen w-screen z-10 inset-0" />
+            <motion.div className="flex flex-col absolute w-screen top-12 left-0 text-center bg-white z-10 font-bold uppercase tracking-[2.5px]">
+              <Link href='/stories'>
+                <a className="py-5">stories</a>
+              </Link>
+              <Link href='/features'>
+                <a className="py-5">features</a>
+              </Link>
+              <Link href='/pricing'>
+                <a className="py-5">pricing</a>
+              </Link>
+              <span className="py-5 inline-block">
+                <Button1 text="get an invite" />
+              </span>
+            </motion.div>
+          </div>
+        )}
+      </AnimatePresence>
+    </>
   )
 }
